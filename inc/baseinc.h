2b2f05b79bfb538ec5a2e9ed4a5e3a9423f8a23c
JJUCAT <17688010148@163.com>
2022-03-02 23:47:02 +0800

    lmrlog

---
+++ inc/baseinc.h
@@ -0,0 +1,10 @@
+#ifndef __BASEINC_H__
+#define __BASEINC_H__
+
+#include "lmrcfg.h"
+#include "lmrtypes.h"
+#include "lmrlog.h"
+#include "obj.h"
+
+
+#endif // __BASEINC_H__
---
+++ inc/clock.h
@@ -0,0 +1,19 @@
+#ifndef __CLOCK_H__
+#define __CLOCK_H__
+
+#include "baseinc.h"
+
+
+namespace lmr {
+
+class Clock : public Obj
+{
+
+}
+
+
+
+
+} // namespace lmr
+
+#endif //  __CLOCK_H__
